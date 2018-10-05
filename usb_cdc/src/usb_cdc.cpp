d47fb3c258f02665561abf85bbb3cb6c6393fef7
Late347 <late347@gmail.com>
2018-10-05 11:11:27 +0300

    added rit logic and new tasks for executing

--- usb_cdc/src/usb_cdc.cpp
+++ usb_cdc/src/usb_cdc.cpp
@@ -39,19 +39,33 @@
 
 //global semaphores and variables
 SemaphoreHandle_t syncSemph;
-volatile uint32_t RIT_count;
-SemaphoreHandle_t sbRIT;
+
+volatile std::atomic<int> executeM1orM2(0); //check 1 or 2 inside isr to decide
+volatile int m1parameter = 0;//decide in another function dirPins based on these results
+volatile int m2parameter = 0;//decide in another function dirPins based on these results
+int octant = 0; //from [1-8]
+int ppsValue=800;
+
+volatile uint32_t RIT_count; //NOTE!! THIS VARIABLE IS NECESSARY!
+SemaphoreHandle_t sbRIT; //NOTE!! THIS SEMAPHORE IS NECESSARY! DOUBLECHECK MAIN() TO SEE IF CREATED CORRECTLY...
 SemaphoreHandle_t sbGo;
 static  std::atomic<bool> calibrationFinished(false);
-//does this pulseState have to be volatile??
-static volatile std::atomic<bool> pulseState(true);
+static volatile std::atomic<bool> pulseState(true);//NOTE!! THIS VARIABLE IS NECESSARY!
 QueueHandle_t commandQueue;
-DigitalIoPin*limit1P;
-DigitalIoPin*limit2P;
-DigitalIoPin*stepP;
-DigitalIoPin*dirP;
 
 
+
+//NOTE!! THESE GLOBAL POINTERS ARE NECESSARY!
+DigitalIoPin *limit1P;
+DigitalIoPin *limit2P;
+DigitalIoPin *limit3P;
+DigitalIoPin *limit4P;
+DigitalIoPin *stepXP;
+DigitalIoPin *dirXP;
+DigitalIoPin *stepYP;
+DigitalIoPin *dirYP;
+DigitalIoPin *laserP;
+DigitalIoPin *penP;
 
 
 
@@ -77,17 +91,35 @@ void RIT_IRQHandler(void) {
 	// Timer then removes the IRQ until next match occurs
 	Chip_RIT_ClearIntStatus(LPC_RITIMER); // clear IRQ flag
 
-	if (limit1P != NULL && limit2P != NULL) {
+	if (limit1P != NULL && limit2P != NULL && limit3P != NULL && limit4P != NULL) {
+
 		if (RIT_count > 0) {
 			RIT_count--;
 			// do something useful here...
-			if (!limit1P->read() && !limit2P->read()) {
+			bool isOK = (  !limit1P->read() && !limit2P->read() && !limit3P->read() && !limit4P->read()  );
+			if (isOK) {
+				if(executeM1orM2==1){
+					//actuate only one motor straight move
+					//execute M1 pattern move
+					switch(m1parameter){
+					case 1: stepXP->write(pulseState);break;
+					case 3: stepYP->write(pulseState);break;
+					case 5:stepXP->write(pulseState); break;
+					case 7:stepYP->write(pulseState);break;
+					default:break;
+					}
+				}else if(executeM1orM2==2){
+					//actuate both motors diagonal move
+					//execute M2 pattern move
+					stepXP->write(pulseState);
+					stepYP->write(pulseState);
+				}
 				//move motor and toggle pulsestate rit_halfpulses
-				stepP->write(pulseState);
+				//stepP->write(pulseState);
 				pulseState = !pulseState;
 			} else {
-				stepP->write(false);
-				dirP->write(!dirP->read());
+				stepXP->write(false);
+				stepYP->write(false);
 				RIT_count = 0; //WE HIT THE WALL, set rit_count=0 and soon the motor will stop, hopefully
 			}
 		} else {
@@ -97,6 +129,7 @@ void RIT_IRQHandler(void) {
 		}
 		// End the ISR and (possibly) do a context switch
 		portEND_SWITCHING_ISR(xHigherPriorityWoken);
+
 	}
 }
 }
@@ -139,6 +172,138 @@ static void prvSetupHardware(void)
 					 * == 1000 microsecs per 1 fullstep
 					 * == 500 microsecs per 0.5 fullstep
  * */
+
+
+int getOctant(bool A, bool B, bool C) {
+	/*function is based on Bresingham's original paper, in the tabulated information section about the algorithm
+	 *
+	 * check booleans to determine octant number
+	later inside adjustm1motor and adjustm2motor
+	we determine those movemeentpatterns with the octant number
+	A = deltaX < 0
+	B = deltaY < 0
+	C = 0 < abs(x1 - x0) - abs(y1 - y0)
+	*/
+	if (!A && !B && !C) {
+		return 1; //oct1
+	}
+	else if (!A && !B && C) {
+		return 2; //oct2
+	}
+	else if (!A && B && !C) {
+		return 8; //oct8
+	}
+	else if (!A && B && C) {
+		return 7; //oct7
+	}
+	else if (A && !B && !C) {
+		return 4;
+	}
+	else if (A && !B && C) {
+		return 3;
+	}
+	else if (A && B && !C) {
+		return 5;
+	}
+	else {
+		return 6;
+	}
+
+
+}
+
+//returns currently active motorpattern (horizontal or vert motormove) and sets pins
+int adjustm1motor(int curOctant) {
+	/*m1 and m1 patter motor movements are based on Bresenham's original paper
+	 * "Algorithm for computer control of a digital plotter (Figure 5)"
+	 * where he describes movements of digital plotter in conjunction with
+	 * the bresenham line algorithm (m1 and m2 patterns are the
+	 * actual paths of plotter)
+	 * typically m1 pattern is either horizontal or vertical move of one motor
+	 * typically m2 pattern is a diagonal move e.g. 45deg angle, which reguires both motors stepping*/
+	int M1pattern = 0;
+	switch (curOctant) {
+		case 1: M1pattern = 1;break;
+		case 2: M1pattern = 3;break;
+		case 3: M1pattern = 3;break;
+		case 4: M1pattern = 5;break;
+		case 5: M1pattern = 5;break;
+		case 6: M1pattern = 7;break;
+		case 7: M1pattern = 7;break;
+		case 8: M1pattern = 1;break;
+
+		 default: break;//shoudlnt be here
+	}
+	switch (M1pattern) {
+		case 1:
+			//XMOTOR RIGHT
+			dirXP->write(true); break;
+		case 3:
+			//YMOTOR UP
+			dirYP->write(true); break;
+		case 5:
+			//XMOTOR LEFT
+			dirXP->write(false); break;
+		case 7:
+			//YMOTOR DOWN
+			dirYP->write(false); break;
+		default://shoudlnt be here
+			break;
+	}
+	m1parameter = M1pattern;
+	return m1parameter;
+
+}
+
+//returns currently active motorpattrern, (diagonal motormove) and sets pins
+int adjustm2motor(int curOctant) {
+	/*m1 and m1 patter motor movements are based on Bresenham's original paper
+	 * "Algorithm for computer control of a digital plotter (Figure 5)"
+	 * where he describes movements of digital plotter in conjunction with
+	 * the bresenham line algorithm (m1 and m2 patterns are the
+	 * actual paths of plotter)
+	 * typically m1 pattern is either horizontal or vertical move of one motor
+	 * typically m2 pattern is a diagonal move e.g. 45deg angle, which reguires both motors stepping*/
+	int M2pattern = 0;
+	switch (curOctant) {
+		case 1: M2pattern = 2;break;
+		case 2: M2pattern = 2;break;
+		case 3: M2pattern = 4;break;
+		case 4: M2pattern = 4;break;
+		case 5: M2pattern = 6;break;
+		case 6: M2pattern = 6;break;
+		case 7: M2pattern = 8;break;
+		case 8: M2pattern = 8;break;
+		default: break; //shoudlnt be here
+	}
+
+	switch(M2pattern){
+
+	case 2: //xmotor right ymotor up
+		dirXP->write(true);
+		dirYP->write(true);
+		break;
+	case 4: //xmotor left ymotor up
+		dirXP->write(false);
+		dirYP->write(true);
+		break;
+	case 6: //xmotor left ymotor down
+		dirXP->write(false);
+		dirYP->write(false);
+		break;
+	case 8: //xmotor right ymotor down
+		dirXP->write(true);
+		dirYP->write(false);
+		break;
+	default: break;
+	}
+
+	m2parameter = M2pattern;
+	return m2parameter;
+}
+
+
+
 void RIT_start(int count, int us) {
 	uint64_t cmp_value;
 	// Determine approximate compare value based on clock rate and passed interval
@@ -165,16 +330,6 @@ void RIT_start(int count, int us) {
 		// unexpected error
 	}
 }
-
-
-
-
-
-
-
-
-
-
 
 
 //EXAMPLE FUNCTION SCTSETUP THREE COUNTERS FOR THREE RGB COLORS
@@ -336,9 +491,284 @@ uint16_t getPeriod(){
 
 
 
+
+//temporary function, assume that 1 fullstep matches 1mm
+//function is needed to convert G1coords into stepamounts
+
+
+//special case for purely horizontal moves BRESENHAM
+void plotLineHoriz(int x0, int y0, int x1, int y1) {
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	//bool drawRight = false;
+
+	executeM1orM2 = 1; //notify isr global variable which patternm1 or patternm2 to execute, in this case only one motor move => hence m1pattern
+	if (dx > 0) {
+		//xmotordir right
+		//drawRight = true;
+		dirXP->write(true);
+		//set xmotor dir right
+		m1parameter = 1; //set global volatile variable to notify isr which m1 motor to actjuate
+	}
+	else{
+		//xmotordir left
+		dirXP->write(false);
+		//set xmotor dir left
+		m1parameter = 5;
+	}
+
+
+	for (int x = 0; x <= abs(dx); x++) {
+		//write to motordir to drive left or right
+		//rit_Start(2, 1000000/(2*pps) )
+		RIT_start(2, 1000000/(2*ppsValue) );
+
+	}
+}
+
+//special case for purely vertical moves BRESENHAM
+void plotLineVert(int x0, int y0, int x1, int y1) {
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	//bool drawUp = false;
+	executeM1orM2 = 1; //notify isr which patternm1 or patternm2 to execute, in this case only one motor move => hence m1pattern
+	//int nabla = 2 * dx - dy; //decision parameter
+	//int x = x0;
+	//int y = y0;
+
+	if (dy > 0) {
+		//drawUp = true;
+		//y motor up
+		m1parameter =3; //notify isr with global variable to expect ymotor up
+		dirYP->write(true);
+	}
+	else{
+		//ymotor down
+		m1parameter =7; //notify isr with global variable to expect ymotor down
+		dirYP->write(false);
+	}
+
+	for (int y = 0; y <= abs(dy); y++) {
+		//write motorpin up or down
+		//rit start (2, 1000000/(2*pps));
+		RIT_start(2, 1000000/(2*ppsValue) );
+	}
+}
+
+//BRESENHAM HELPER FUNCTION
+void plotLineLow(int x0, int y0, int x1, int y1) {
+
+	/*setting up required global variables for algorithm:
+	interrupt handler uses following global variables:
+	volatile int dx
+	volatile int dy
+	volatile int nabla //decisionparameter
+	volatile int stepCount
+	volatile int RIT_Counter = abs(dx) //it means x is driving axis
+	start rit_counter with 2*abs(dx)
+	*/
+
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int yincr = 1;
+
+	if (dy < 0) {
+		yincr = -1;
+		dy = -dy;
+	}
+
+	int nabla = 2 * dy - dx; //decision parameter
+	int y = y0;
+	int x = x0;
+
+	/*instead of loop running with stepping, start rit_counter
+	with driving axis value such as... ritcount = 2*  ( convert to steps the following  abs(drivingaxiscoord1-drivingaxiscoord0))
+	rit_count stops when reaches value zero (decrement in isr)
+	 //2x beacuse halfstepping with isr
+	 stepcount = 0.5 * ritcount
+	*/
+
+	for (x; x <= x1; x++) { //x is driving axis
+		/*plot x,y OR MOVE MOTORS???*/
+		if (nabla > 0) {
+			y = y + yincr;
+			nabla = nabla - 2 * dx;//update decisionparameter nabla
+			/*execute diagonal m2 move (step x and y motor at same time)???
+			 m2 move type depends on the octant
+			m1 move type depends on octant also (m1 is either horiz or vert move)*/
+			executeM1orM2 = 2; //set global volatile boolean, so that interrupthandler knows which movementpattern to execute
+			//rit_Start (2, (1000000 / (2*pps)) )
+			RIT_start(2, 1000000/(2*ppsValue) );
+		}
+		else {
+			//execute m1 move
+			executeM1orM2 = 1;
+			//rit_Start (2, (1000000 / (2*pps)) )
+			RIT_start(2, 1000000/(2*ppsValue) );
+		}
+
+		nabla = nabla + 2 * dy;//update decisionparameter nabla
+	}
+}
+
+//BRESENHAM HELPER FUNCTION
+void plotLineHigh(int x0, int y0, int x1, int y1) {
+
+	/*setting up required global variables for algorithm:
+	interrupt handler uses following global variables:
+	volatile int dx
+	volatile int dy
+	volatile int nabla //decisionparameter
+	volatile int stepCount
+	volatile int RIT_Counter = abs(dy) //it means y is driving axis
+	start rit_counter with 2*abs(dy)
+	*/
+
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int xincr = 1;
+
+	if (dx < 0) {
+		xincr = -1;
+		dx = -dx;
+	}
+
+	int nabla = 2 * dx - dy; //decision parameter
+	int x = x0;
+	int y = y0;
+
+	/*instead of loop running with stepping, start rit_counter
+	with driving axis value such as... ritcount = 2*  ( convert to steps the following  abs(drivingaxiscoord1-drivingaxiscoord0))
+	rit_count stops when reaches value zero (decrement in isr)
+	 //2x beacuse halfstepping with isr
+	 stepcount = 0.5 * ritcount
+	*/
+
+	for (y; y <= y1; y++) { //y is driving axis
+		/*plot xy OR MOVE MOTORS??? how the f**k we move motors or 1 motor per loop iteration? */
+		if (nabla > 0) {
+			x += xincr;
+			nabla = nabla - 2 * dy;//update decisionparameter nabla
+			//execute diagonal m2 move (step x and y motor at same time)
+			// m2 move type depends on the octant
+			//m1 move type depends on octant also (m1 is either horiz or vert move)
+			executeM1orM2 = 2;
+			//rit_Start (2, (1000000 / (2*pps)) )
+			RIT_start(2, 1000000/(2*ppsValue) );
+		}
+		else {
+			//execute m1 move
+			executeM1orM2 = 1;
+			//rit_Start (2, (1000000 / (2*pps)) )
+			RIT_start(2, 1000000/(2*ppsValue) );
+		}
+		nabla = nabla + 2 * dx;//update decisionparameter nabla
+	}
+}
+
+//BRESENHAM MAIN FUNCTION
+void plotLineGeneral(int x0, int y0, int x1, int y1) {
+	/*octant notation goes from
+	angle 0deg to 45deg == oct 1
+	angle 45deg to 90deg == oct 2
+	etc...
+	*/
+
+	if (x0 == x1) {
+		//plot vertical case
+		plotLineVert(x0, y0, x1, y1);
+	}
+	else if (y0 == y1) {
+		//plot horizontal case
+		plotLineHoriz(x0, y0, x1, y1);
+	}
+
+	else {
+		bool res = abs(y1 - y0) < abs(x1 - x0);
+		//octant getting is based on bresenhams original paper and usage of helper function to get the correct octant
+		octant = 1;
+		octant = getOctant( (x1-x0<0), (y1-y0<0), (res) );
+		//m1parameter and m2parameters are based on bresenhams original paper with  single motormove and diagonal (dual motor)move
+		 m2parameter = adjustm2motor(octant);
+		 m1parameter = adjustm1motor(octant);
+
+
+		if (res) {
+			if (x0 > x1) {
+				//octant 4, octant 5???
+				plotLineLow(x1, y1, x0, y0);
+			}
+			else {
+				//no swap
+				//octant 1, octant 8???
+				plotLineLow(x0, y0, x1, y1);
+			}
+		}
+		else {
+			if (y0 > y1) {
+				// octant 6, octant 7 ???
+				plotLineHigh(x1, y1, x0, y0);
+			}
+			else {
+				//no swap
+				// octant 2, octant 3 ???
+				plotLineHigh(x0, y0, x1, y1);
+			}
+		}
+	}
+}
+
+
+//executes G1 and pen and laser commands
+static void execute_task (void*pvParameters) {
+
+	//laser and pen pins, hopefully correct???
+	DigitalIoPin laser(0,12,DigitalIoPin::output, true);
+	DigitalIoPin pen(0,10, DigitalIoPin::pullup, true);
+
+	//limit pins
+	DigitalIoPin limit2(0, 0, DigitalIoPin::pullup, true);
+	DigitalIoPin limit1(1, 3, DigitalIoPin::pullup, true);
+	DigitalIoPin limit3(0, 9, DigitalIoPin::pullup, true);
+	DigitalIoPin limit4(0, 29, DigitalIoPin::pullup, true);
+
+	//movement pins for axes
+	DigitalIoPin dirX(0, 28, DigitalIoPin::output, true);
+	DigitalIoPin stepX(0, 27, DigitalIoPin::output, true);
+	DigitalIoPin dirY(1, 0, DigitalIoPin::output, true);
+	DigitalIoPin stepY(0, 24, DigitalIoPin::output, true);
+
+	//assign global pointers
+	limit1P = &limit1;
+	limit2P = &limit2;
+	limit3P = &limit3;
+	limit4P = &limit4;
+	stepXP = &stepX;
+	dirXP = &dirX;
+	stepYP = &stepY;
+	dirYP = &dirY;
+	laserP = &laser;
+	penP = &pen;
+
+	CommandStruct curcmd{CommandStruct::M1, 0, false, 0, 0};
+
+	vTaskDelay(50);
+
+	for(;;){
+		xQueueReceive(commandQueue, &curcmd, portMAX_DELAY); //get command from queue
+		if(curcmd.commandWord == CommandStruct::G1 && curcmd.isLegal){
+			curcmd.xCoord /= (int)100; //make coords into steps
+			curcmd.yCoord /= (int)100; // make coords into steps
+			plotLineGeneral();
+
+		}
+	}
+
+}
+
 //PARSER TASK gets USB-receive and parses the Gcode commands
 static void parse_task(void*pvParameters){
-	bool LedState = false;
+	//bool LedState = false;
 	GcodeParser parser;
 	const int allocsize=80+1;
 	char initialMessage[]="M10 XY 380 310 0.00 0.00 A0 B0 H0 S80 U160 D90\r\nOK\r\n";
@@ -349,6 +779,10 @@ static void parse_task(void*pvParameters){
 	std::string temp="";
 	std::string cppstring="";
 	std::string gcode="";
+
+	//small initial delay
+	vTaskDelay(75);
+
 	while (1) {
 		//str buffer is allocated
 		char str[allocsize]{0};
@@ -359,49 +793,51 @@ static void parse_task(void*pvParameters){
 		cppstring += std::string(str);
 		/*find enterIndex*/
 		auto enterInd = cppstring.find('\n',0);
-		//vTaskDelay(10); //vtaskdelay is used for purpose of itmprint not bugging-out
+		vTaskDelay(10); //vtaskdelay is used for purpose of itmprint not bugging-out
 		if (enterInd != std::string::npos) {
 			/*found entersign, get one line of gcode*/
-			gcode = cppstring.substr(0, enterInd + 1);
-
+			gcode = cppstring.substr(0, enterInd );
 			/*erase the gotten gcodeline, from the cppstring front portion, BECAUSE WE PROCESSED IT ALREADY ==>sent to parser*/
 			cppstring.erase(0, enterInd + 1);
-
 			/*send the gcode string into the parser
-			 * ACTUALLY must remove entersign and cr from the gcodestring first */
-			gcode.erase(enterInd,1);
+			 * ACTUALLY must remove entersign and cr from the gcodestring first
+			 * gcode.erase(enterInd,1) */
 
 			ITM_write(gcode.c_str());
 			ITM_write("\r\n");
-			vTaskDelay(20);
+			//vTaskDelay(20);
 			CommandStruct cmd {CommandStruct::M1, -2147483648, false,-2147483648,-2147483648 };
 			cmd = parser.parseCommand(gcode);
+
 			if(cmd.commandWord==CommandStruct::M10 && cmd.isLegal){
 				USB_send((uint8_t*) initialMessage, initlen );
 			}
 			else if(cmd.isLegal ){
-				//other legal message send ok
+				//any legal message send ok, but also send command to queue
+				xQueueSendToBack(commandQueue, &cmd, portMAX_DELAY);
 				USB_send((uint8_t*) okMessage, oklen );
 			}
 			else {
 				USB_send((uint8_t*) badMessage, strlen(badMessage) );
 			}
-			//NOTE important TO CLEAR TOKENS from parser AFTER EACH ROUND!! otherwise buggy behavior
-
-			/*EDITED parseCommand has clearTokens() at the end
-			parser.clearTokens();*/
+			//NOTE parseCommand memberfuncion clears tokens automatically now!
 			memset(str, 0, allocsize);
-			Board_LED_Set(1, LedState);
-			LedState = (bool) !LedState;
+			//Board_LED_Set(1, LedState);
+			//LedState = (bool) !LedState;
 			gcode="";
 		}
 		else{
 		//else, YOU DIDNT FIND COMPLETE GCODE, WAIT FOR MORE STRBUFFER APPENDED CHARS!
 		}
 	}
+
 }
 
 
+
+static void calibrate_task(void*pvParameters){
+
+}
 
 
 
